9a075867db80f29d1d02d27a5d65be9511f4d33f
Derry Coffey <d.coffey@zengenti.com>
2023-01-27 16:07:11 +0000

    fix: Added optional path to setCookieValue. Added clearCookie function

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,15 +1,26 @@
+const canUseCookies = () => {
+  return typeof window !== "undefined" && navigator.cookieEnabled;
+};
+
+const getExpirationDate = (expiresInDays: number) => {
+  const d = new Date();
+  d.setTime(d.getTime() + expiresInDays * 24 * 60 * 60 * 1000);
+
+  return "; expires=" + d.toUTCString();
+};
+
 /**
  * Get all cookies as key/value pairs
  */
 export const getCookies = (): { [key: string]: string } => {
-  if (typeof window === 'undefined') return {};
+  if (!canUseCookies()) return {};
 
   const cookies = document.cookie?.trim();
 
   const cookiesObj: { [key: string]: string } =
     cookies.length > 0
-      ? cookies.split(';').reduce((obj, val) => {
-          const parts = val.split('=');
+      ? cookies.split(";").reduce((obj, val) => {
+          const parts = val.split("=");
           const key = parts[0].trim();
           const value = parts[1].trim();
           return {
@@ -31,27 +42,32 @@ export const getCookieValue = (key: string): string => {
 };
 
 /**
+ * Clear cookie value by key
+ */
+export const clearCookie = (key: string) => {
+  if (!canUseCookies()) return;
+  const expiration = getExpirationDate(0);
+
+  document.cookie = `${key} = ${expiration}`;
+};
+
+/**
  * Set cookie value
  */
 export const setCookieValue = (
   key: string,
   value: string,
-  expiresInDays?: number
+  expiresInDays?: number,
+  path?: string
 ): void => {
-  if (typeof window === 'undefined') return;
+  if (typeof window === "undefined") return;
 
   // Check browser cookies enables
   if (!navigator.cookieEnabled) return;
 
-  // Clear cookie
-  if (expiresInDays === 0) document.cookie = `${key} = ${value}`;
-  // Set cookie with experiation in days
-  else {
-    const d = new Date();
-    d.setTime(d.getTime() + (expiresInDays || 1) * 24 * 60 * 60 * 1000);
+  const cookiePath = path ? `; path = ${path}` : "";
+  const expiration =
+    expiresInDays === 0 ? "" : getExpirationDate(expiresInDays || 1);
 
-    const expires = 'expires=' + d.toUTCString();
-
-    document.cookie = `${key} = ${value}; ${expires}`;
-  }
+  document.cookie = `${key} = ${value}${expiration}${cookiePath}`;
 };
